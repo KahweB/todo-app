188ffd08a43ff3ba4e6651d5a779f6d2e3c53d05
kahwe <72060765+KahweB@users.noreply.github.com>
2023-08-02 22:44:46 +0300

    webpack

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import './style.css';
+
+
+
+function component() {
+    
+  
+    // Lodash, currently included via a script, is required for this line to work
+    
+    console.log("hello")
+    
+    
+
+    
+  
+    
+  }
+  
+  component();
+  
